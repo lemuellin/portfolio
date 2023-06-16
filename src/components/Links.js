5a0be8db3737c111128109acc59ae1bda1f9fe91
lemuellin <lemuellin@gmail.com>
2023-06-15 19:34:25 -0700

    edit link

--- src/components/Links.js
+++ src/components/Links.js
@@ -5,7 +5,7 @@ import resumePDF from '../assets/Lemuel_Lin_Resume.pdf';
 const Links = () => {
     return(
         <div className="flex gap-5 justify-center items-center">
-            <i onClick={()=>window.open("https://www.linkedin.com/in/lemuel-lin/")} className="devicon-linkedin-plain text-5xl text-center cursor-pointer hover:scale-125"></i>
+            <i onClick={()=>window.open("https://www.linkedin.com/in/jake-d-lin/")} className="devicon-linkedin-plain text-5xl text-center cursor-pointer hover:scale-125"></i>
             <i onClick={()=>window.open("https://github.com/lemuellin")} className="devicon-github-original colored text-5xl text-center cursor-pointer hover:scale-125"></i>
             <img onClick={() => window.open("mailto:lemuellin@gmail.com")} className="w-12 cursor-pointer hover:scale-125" src={Email} alt={"email icon"}/>
             <img onClick={() => window.open(resumePDF)} className="w-12 cursor-pointer hover:scale-125" src={Resume} alt={"resume icon"}/>
